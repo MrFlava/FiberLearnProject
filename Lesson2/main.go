234cd1f09c3ac8fe9ed97b8109cc8c010ef101ad
MrFlava <thatelitemaili33t@gmail.com>
2024-10-10 21:51:15 +0300

    app configs

--- Lesson2/main.go
+++ Lesson2/main.go
@@ -12,10 +12,13 @@ func getHandler(c *fiber.Ctx) error {
 
 func main() {
 	appConfig := fiber.Config{
-		AppName: "Some interesting API",
+		AppName:           "Some interesting API",
+		EnablePrintRoutes: true,
+		ServerHeader:      "Test API 1",
+		Prefork:           true,
 	}
 	app := fiber.New(appConfig)
-	app.Get("/", getHandler)
+	app.Get("/", getHandler).Name("Get default")
 
 	log.Info("Starting server on port 3000")
 	err := app.Listen(":3000")
