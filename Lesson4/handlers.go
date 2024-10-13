6d2efd094d5e8e4bccd0fa9868fec0a182e3390c
MrFlava <thatelitemaili33t@gmail.com>
2024-10-13 17:06:37 +0300

    Add new enpoints and handlers for them

--- Lesson4/handlers.go
+++ Lesson4/handlers.go
@@ -6,7 +6,29 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-func getAllBooksHnalder(c *fiber.Ctx) error {
-	slog.Info("request to get all books")
+func getAllBooksLCHnalder(c *fiber.Ctx) error {
+	slog.Info("request to get all books - lovwercase")
+	return nil
+}
+
+func getAllBooksUCHnalder(c *fiber.Ctx) error {
+	slog.Info("request to get all books - uppercase")
+	return nil
+}
+
+func getBookByIdHandler(c *fiber.Ctx) error {
+	bookId := c.Params("id")
+	slog.Info("request to get book by ID", "bookId", bookId)
+	return nil
+}
+
+func getAuthorsOrAuthorByIdHandler(c *fiber.Ctx) error {
+	authorId := c.Params("id")
+
+	if authorId == "" {
+		slog.Info("request to get all authors")
+	}
+
+	slog.Info("request to get author by ID", "authorId", authorId)
 	return nil
 }
--- Lesson4/main.go
+++ Lesson4/main.go
@@ -10,10 +10,14 @@ func main() {
 	appConfig := fiber.Config{
 		Immutable:         true,
 		EnablePrintRoutes: true,
+		CaseSensitive:     true,
 	}
 
 	app := fiber.New(appConfig)
-	app.Get("/books", getAllBooksHnalder).Name("Get all boks")
+	app.Get("/books", getAllBooksLCHnalder).Name("Get all boks lowercase")
+	app.Get("/Books", getAllBooksUCHnalder).Name("Get all boks uppercase")
+	app.Get("/books/:id", getBookByIdHandler).Name("Get book by ID")
+	app.Get("/author/:id?", getAuthorsOrAuthorByIdHandler).Name("Get all authors or a specific author")
 
 	err := app.Listen(":3000")
 	if err != nil {
